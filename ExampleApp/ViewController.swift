5f4b2d0bd422380b354a90491b474dd0626de271
Smirnov Sergey <fost@live.ru>
2024-09-30 22:55:19 +0300

    Добавлен UIStackView

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 	private let textLabel = UILabel()
     private let imageConteinerView = UIView()
     private let imageView = UIImageView()
+    private let stackView = UIStackView()
 
 
 	override func viewDidLoad() {
@@ -22,9 +23,11 @@ class ViewController: UIViewController {
 		setupLabel()
         setupImageConteinerView()
 		setupImageView()
-        view.addSubview(textLabel)
-        view.addSubview(imageConteinerView)
         setupView()
+        setupStackView()
+//        view.addSubview(textLabel)
+//        view.addSubview(imageConteinerView)
+        view.addSubview(stackView)
         setupLayout()
 	}
 	
@@ -36,6 +39,7 @@ class ViewController: UIViewController {
 		let firstNumber = helper.getNumbers().first
 		textLabel.text = "\(firstNumber ?? 0)"
 		textLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textLabel.textAlignment = .center
 		textLabel.textColor = .red
 	}
     
@@ -65,21 +69,41 @@ class ViewController: UIViewController {
         view.layer.insertSublayer(gradient, at: 0)
     }
     
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        
+        // Добавляем элементы в StackView
+        stackView.addArrangedSubview(textLabel)
+        stackView.addArrangedSubview(imageConteinerView)
+    }
+    
     private func setupLayout() {
-        textLabel.translatesAutoresizingMaskIntoConstraints = false
+//        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         imageConteinerView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            textLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageConteinerView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 150),
-            imageConteinerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageConteinerView.heightAnchor.constraint(equalToConstant: 200),
-            imageConteinerView.widthAnchor.constraint(equalToConstant: 200),
-            imageView.leftAnchor.constraint(equalTo: imageConteinerView.leftAnchor),
+            // Ограничения для stackView
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
+            stackView.heightAnchor.constraint(equalToConstant: 400),
+            
+//            textLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+//            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            imageConteinerView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 150),
+//            imageConteinerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            imageConteinerView.heightAnchor.constraint(equalToConstant: 200),
+//            imageConteinerView.widthAnchor.constraint(equalToConstant: 200),
+
+            // Ограничения для imageView внутри imageConteinerView
+            imageView.leadingAnchor.constraint(equalTo: imageConteinerView.leadingAnchor),
             imageView.topAnchor.constraint(equalTo: imageConteinerView.topAnchor),
-            imageView.rightAnchor.constraint(equalTo: imageConteinerView.rightAnchor),
+            imageView.trailingAnchor.constraint(equalTo: imageConteinerView.trailingAnchor),
             imageView.bottomAnchor.constraint(equalTo: imageConteinerView.bottomAnchor)
         ])
     }
