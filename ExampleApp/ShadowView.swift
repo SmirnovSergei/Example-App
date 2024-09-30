261f0840286fb1f392116b5ab60699363cded0a2
Smirnov Sergey <fost@live.ru>
2024-10-01 00:35:46 +0300

    Внесены изменения в соответствие с 17-м уроком

---
+++ ExampleApp/ShadowView.swift
@@ -0,0 +1,60 @@
+//
+//  ShadowView.swift
+//  ExampleApp
+//
+//  Created by Сергей Смирнов on 30.09.2024.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    private let imageView = UIImageView()
+
+/*
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupImageView()
+        setupLayout()
+    }
+ */
+    init(imageName: String) {
+        super.init(frame: .zero)
+        setupView()
+        setupImageView(imageName)
+        setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupImageView(_ imageName: String) {
+        imageView.image = UIImage(named: imageName)
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        
+        addSubview(imageView)
+    }
+
+    private func setupView() {
+        layer.cornerRadius = 20
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize(width: 5, height: 5)
+        layer.shadowRadius = 10
+    }
+    
+    private func setupLayout() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController {
 	
 	private let helper = Helper()
 	private let textLabel = UILabel()
-    private let imageConteinerView = UIView()
-    private let imageView = UIImageView()
+    private let shadowView = ShadowView(imageName: "raccoon")
+    private let secondShadowView = ShadowView(imageName: "custom")
     private let stackView = UIStackView()
 
 
@@ -21,12 +21,8 @@ class ViewController: UIViewController {
 		updateNumbers()
 		
 		setupLabel()
-        setupImageConteinerView()
-		setupImageView()
         setupView()
         setupStackView()
-//        view.addSubview(textLabel)
-//        view.addSubview(imageConteinerView)
         view.addSubview(stackView)
         setupLayout()
 	}
@@ -42,23 +38,7 @@ class ViewController: UIViewController {
         textLabel.textAlignment = .center
 		textLabel.textColor = .red
 	}
-    
-    private func setupImageConteinerView() {
-        imageConteinerView.layer.shadowColor = UIColor.black.cgColor
-        imageConteinerView.layer.shadowOffset = CGSize(width: 15, height: 15)
-        imageConteinerView.layer.shadowOpacity = 1
-        imageConteinerView.layer.shadowRadius = 10
-    }
-    
-    private func setupImageView() {
-        imageView.image = UIImage(named: "raccoon")
-        imageView.layer.cornerRadius = 20
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFit
-        
-        imageConteinerView.addSubview(imageView)
-    }
-    
+
     private func setupView() {
         let gradient = CAGradientLayer()
         gradient.frame = view.bounds
@@ -68,7 +48,7 @@ class ViewController: UIViewController {
 
         view.layer.insertSublayer(gradient, at: 0)
     }
-    
+
     private func setupStackView() {
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
@@ -77,34 +57,19 @@ class ViewController: UIViewController {
         
         // Добавляем элементы в StackView
         stackView.addArrangedSubview(textLabel)
-        stackView.addArrangedSubview(imageConteinerView)
+        stackView.addArrangedSubview(shadowView)
+        stackView.addArrangedSubview(secondShadowView)
     }
-    
+
     private func setupLayout() {
-//        textLabel.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        imageConteinerView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         NSLayoutConstraint.activate([
             // Ограничения для stackView
             stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.widthAnchor.constraint(equalToConstant: 200),
-            stackView.heightAnchor.constraint(equalToConstant: 400),
-            
-//            textLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-//            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            imageConteinerView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 150),
-//            imageConteinerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            imageConteinerView.heightAnchor.constraint(equalToConstant: 200),
-//            imageConteinerView.widthAnchor.constraint(equalToConstant: 200),
-
-            // Ограничения для imageView внутри imageConteinerView
-            imageView.leadingAnchor.constraint(equalTo: imageConteinerView.leadingAnchor),
-            imageView.topAnchor.constraint(equalTo: imageConteinerView.topAnchor),
-            imageView.trailingAnchor.constraint(equalTo: imageConteinerView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: imageConteinerView.bottomAnchor)
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
         ])
     }
 }
